485a4f8ab0bb3f18edb6dc702eafe4c5a1d822d8
Zygorg <43007952+OfficialZygorg@users.noreply.github.com>
2022-05-16 21:19:16 +0200

    Temporally patch to make it work

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,12 +1,13 @@
 //Game Data  
 var player = {
-  particles: 100,
+  particles: 10,
   result: 0,
   lastUpdate: Date.now(),
   particleColliders:[],
   implosion:{
     cost: 10*10e1,
     implodedParticles: 0,
+    extraParticleColliders: 0,
   }, 
   options:{
     notation: "Mixed scientific",
@@ -28,44 +29,15 @@ function openTab(evt, idName) {
   evt.currentTarget.className += " active";
 }
 
-for (i=0; i<=11; i++) {
+for (i=1; i<=12; i++) {
   let particleCollider = {
-    cost: Math.pow(Math.pow(10,i),i)*10,
+    cost: Math.pow(10,i),
     bought: 0,
     multiplier: 1, 
     ammount: 0,
   }
   player.particleColliders.push(particleCollider);
 }
-
-function updateGUI(){
-  document.getElementById("particlesAmmount").textContent = "You have: "+formatValue(player.options.notation,player.particles,2,1)+" particles";
-  for (i=0; i<player.particleColliders.length; i++) {
-    document.getElementById("particleCollider"+i).innerHTML = "Ammount: " + formatValue(player.options.notation,player.particleColliders[i].ammount,2,1) +" Cost: "+formatValue(player.options.notation,player.particleColliders[i].cost,2,1)+" ("+player.particleColliders[i].bought+" Bought. Multiplier of "+formatValue(player.options.notation,player.particleColliders[i].multiplier,2,1)+")";
-  }
-}
-
-function productionLoop(diff){
-  player.particles += player.particleColliders[0].ammount * player.particleColliders[0].multiplier * diff;
-  for (let i=1; i<player.particleColliders.length; i++) {
-    player.particleColliders[i-1].ammount += player.particleColliders[i-1].ammount * player.particleColliders[i-1].multiplier * diff / 5; 
-  }
-}
-
-function mainLoop(diff){
-  var diff = (Date.now - player.lastUpdate) / 1000;
-  productionLoop(diff);
-  updateGUI();particles();
-  player.lastUpdate = Date.now();
-}
-setInterval(mainLoop, 50);
-
-function revealColliders(){for (i=0; i<=12; i++){if(player.implosion.particleColliders >= 1){if(player.particles >= player.particleColliders.particleCollider+[i].cost){document.getElementById("particleCollider[i]").style.display = "block";}}}};
-function implodeReality(){if(player.particles >= player.implosion.implodedCost){document.getElementById("implodedRealityButton").display = block;}}
-function revealImplodeRealityTab(){if (player.implosion.implodedParticles >= 1){document.getElementsByClassName("implodedReality").hidden = false;}}
-function reveals(){
-  implodeReality();revealImplodeRealityTab();
-}setInterval(reveals, 50);
 
 function buyGenerators(i){
   if (player.particleColliders[i].cost>player.particles) return;
@@ -75,3 +47,31 @@ function buyGenerators(i){
   player.particleColliders[i].multiplier *= 1.05
   player.particleColliders[i].cost *= 1.5
 }
+
+function updateGUI(){
+  document.getElementById("particlesAmmount").textContent = "You have: "+formatValue(player.options.notation,player.particles,2,1)+" particles";
+  document.getElementById("particleCollider0").innerHTML = "Buy Particle Collider "+1+"<br> Ammount: " + formatValue(player.options.notation,player.particleColliders[0].ammount,2,1) +"<br> Cost: "+formatValue(player.options.notation,player.particleColliders[0].cost,2,1)+"<br> Bought: "+player.particleColliders[0].bought+"<br> Multiplier: "+formatValue(player.options.notation,player.particleColliders[0].multiplier,2,1);
+  for (i=1; i<player.particleColliders.length; i++) {
+    document.getElementById("particleCollider"+i).innerHTML = "Buy Particle Collider "+(i+1)+"<br> Ammount: " + formatValue(player.options.notation,player.particleColliders[i].ammount,2,1) +"<br> Cost: "+formatValue(player.options.notation,player.particleColliders[i].cost,2,1)+"<br> Bought: "+player.particleColliders[i].bought+"<br> Multiplier: "+formatValue(player.options.notation,player.particleColliders[i].multiplier,2,1);
+  }
+}
+
+function productionLoop(diff){
+  player.particles += player.particleColliders[0].ammount * player.particleColliders[0].multiplier * diff;
+  for (let i=1; i<player.particleColliders.length; i++) {
+    player.particleColliders[i].ammount += player.particleColliders[i].ammount * player.particleColliders[i].multiplier * diff / 5; 
+  }
+}
+
+function mainLoop(){
+  var diff = (Date.now() - player.lastUpdate) / 1000;
+  productionLoop(diff);updateGUI();
+  player.lastUpdate = Date.now();
+}
+setInterval(mainLoop, 50);
+
+function implodeReality(){if(player.particles >= player.implosion.implodedCost){document.getElementById("implodedRealityButton").display = block;}}
+function revealImplodeRealityTab(){if (player.implosion.implodedParticles >= 1){document.getElementsByClassName("implodedReality").hidden = false;}}
+function reveals(){
+  implodeReality();revealImplodeRealityTab();
+}setInterval(reveals, 50);
