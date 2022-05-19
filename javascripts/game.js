43a675adeefb81dd7d37c2afa5a75414dc2c392e
Zygorg <43007952+OfficialZygorg@users.noreply.github.com>
2022-05-19 21:43:28 +0200

    Messed with the code

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,7 +1,6 @@
 //Game Data  
 var player = {
   particles: 10,
-  particlesLimit: 1000,
   result: 0,
   lastUpdate: Date.now(),
   particleColliders:[],
@@ -30,32 +29,55 @@ function openTab(evt, idName) {
   evt.currentTarget.className += " active";
 }
 
-for (i=1; i<=12; i++) {
-  let particleCollider = {
-    cost: Math.pow(10,i),
-    bought: 0,
-    multiplier: 1, 
-    ammount: 0,
+function valBetween(v, min, max) {
+  return (Math.min(max, Math.max(min, v)));
+}
+
+function showDiv(divId) {
+  var x = document.getElementById(divId);
+  if (x.style.display === "none") {
+    x.style.display = "block";
   }
-  player.particleColliders.push(particleCollider);
+}
+
+function show(id){
+  document.getElementById(id).removeAttribute("hidden");
+}
+
+function makeColliders(){
+  for (i=1; i<=12; i++) {
+    let particleCollider = {
+      cost: Math.pow(10,i),
+      bought: 0,
+      multiplier: 1, 
+      ammount: 0,
+    }
+    player.particleColliders.push(particleCollider);
+  }
+}makeColliders()
+
+function particlesPS(){
+  for (i=0; i<1; i++) {
+    document.getElementById("particlesPerSecond").innerHTML = "Particles per second: "+formatValue(player.options.notation,player.particleColliders[i].ammount * player.particleColliders[i].multiplier,2,2);
+  }
+  for(i=2;i<player.implosion.timesImploded;i++){
+    document.getElementById("particlesPerSecond").innerHTML += "Particles per second: "+formatValue(player.options.notation,player.particleColliders[i].ammount * player.particleColliders[i].multiplier,2,2);
+  }
+}
+
+function canBuy(){
+  for (i=0; i<player.particleColliders.length; i++) {
+    if (player.particles>=player.particleColliders[i].cost) {document.getElementById("particleCollider"+i).style.borderColor = "green";}else {document.getElementById("particleCollider"+i).style.borderColor = "red";}
+    if(player.particles>=player.implosion.cost){document.getElementById("implodedRealityButton").style.borderColor = "green";}else {document.getElementById("implodedRealityButton").style.borderColor = "red";}
+  }
 }
 
 function particlesLimitedAmmount() {
-  if (player.particles > player.particlesLimit){
-    player.particles = player.particlesLimit;
-    if (player.implosion.timesImploded > 0){
-      player.particlesLimit = player.particlesLimit * (2 * player.implosion.timesImploded);
-    }
+  if (player.particles >= player.implosion.cost) {
+    player.particles = valBetween(player.particles, 0, player.implosion.cost);
   }
-  document.getElementById("particlesLimit").innerHTML = "Particles Limit: " + formatValue(player.options.notation,player.particlesLimit,2,1);
-}
-
-function particlesPerSecond(){
-  for (i=0; i<player.particleColliders.length; i++){
-  player.result = player.particles / (player.particleColliders[i].ammount * player.particleColliders[i].multiplier); 
-  }
-  document.getElementById("particlesPerSecond").innerHTML = "Particles per second: " + formatValue(player.options.notation,player.result,2,1);
-}
+  document.getElementById("particlesLimit").innerHTML = "Particles Limit: " + formatValue(player.options.notation,player.implosion.cost,2,1);
+}setInterval(particlesLimitedAmmount, 1);
 
 function buyGenerators(i){
   if (player.particleColliders[i].cost>player.particles) return;
@@ -67,10 +89,10 @@ function buyGenerators(i){
 }
 
 function updateGUI(){
-  document.getElementById("particlesAmmount").textContent = "You have: "+formatValue(player.options.notation,player.particles,2,1)+" particles";
-  document.getElementById("particleCollider0").innerHTML = "Buy Particle Collider "+1+"<br> Ammount: " + formatValue(player.options.notation,player.particleColliders[0].ammount,2,1) +"<br> Cost: "+formatValue(player.options.notation,player.particleColliders[0].cost,2,1)+"<br> Bought: "+player.particleColliders[0].bought+"<br> Multiplier: "+formatValue(player.options.notation,player.particleColliders[0].multiplier,2,2);
+  document.getElementById("particlesAmmount").textContent = "You have: "+formatValue(player.options.notation,player.particles,2,2)+" particles";
+  document.getElementById("particleCollider0").innerHTML = "Buy Particle Collider "+1+"<br> Ammount: " + formatValue(player.options.notation,player.particleColliders[0].ammount,2,2) +"<br> Cost: "+formatValue(player.options.notation,player.particleColliders[0].cost,2,2)+"<br> Bought: "+player.particleColliders[0].bought+"<br> Multiplier: "+formatValue(player.options.notation,player.particleColliders[0].multiplier,2,2);
   for (i=1; i<player.particleColliders.length; i++) {
-    document.getElementById("particleCollider"+i).innerHTML = "Buy Particle Collider "+(i+1)+"<br> Ammount: " + formatValue(player.options.notation,player.particleColliders[i].ammount,2,1) +"<br> Cost: "+formatValue(player.options.notation,player.particleColliders[i].cost,2,1)+"<br> Bought: "+player.particleColliders[i].bought+"<br> Multiplier: "+formatValue(player.options.notation,player.particleColliders[i].multiplier,2,2);
+    document.getElementById("particleCollider"+i).innerHTML = "Buy Particle Collider "+(i+1)+"<br> Ammount: " + formatValue(player.options.notation,player.particleColliders[i].ammount,2,2) +"<br> Cost: "+formatValue(player.options.notation,player.particleColliders[i].cost,2,2)+"<br> Bought: "+player.particleColliders[i].bought+"<br> Multiplier: "+formatValue(player.options.notation,player.particleColliders[i].multiplier,2,2);
   }
 }
 
@@ -83,22 +105,33 @@ function productionLoop(diff){
 
 function mainLoop(){
   var diff = (Date.now() - player.lastUpdate) / 1000;
-  productionLoop(diff);updateGUI();particlesLimitedAmmount();particlesPerSecond();
+  productionLoop(diff);updateGUI();implodedParticlesShow();particlesPS();
   player.lastUpdate = Date.now();
 }
 setInterval(mainLoop, 50);
 
-function showDiv(divId) {
-  var x = document.getElementById(divId);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }
+function implodedParticlesShow(){
+  document.getElementById("implodedRealityButton").innerHTML = "Implode Reality<br>Gain 1 imploded particle<br> Cost: "+formatValue(player.options.notation,player.implosion.cost,2,1)+" particles";
 }
 
 
-function revealParticleColliders(){for (i=1; i<player.particleColliders.length; i++) {if (player.particles >= player.particleColliders[i].cost) {showDiv("particleCollider"+i);}}}
-function implodeReality(){if(player.particles >= player.implosion.implodedCost){document.getElementById("implodedRealityButton").display = block;}}
-function revealImplodeRealityTab(){if (player.implosion.implodedParticles >= 1){document.getElementsByClassName("implodedReality").hidden = false;}}
+function implodeReality(){
+  if (player.particles>=player.implosion.cost) {
+    player.particles -= player.implosion.cost;
+    player.implosion.implodedParticles += 1;
+    player.implosion.timesImploded += 1;
+    player.implosion.cost *= (2 * player.implosion.timesImploded);
+    player.particles = 10,
+    player.result = 0,
+    player.lastUpdate = Date.now()
+    player.particleColliders=[]
+    makeColliders();
+  }
+}
+
+function revealParticleColliders(){for(i=2;i<player.implosion.timesImploded;i++){if(player.particles>=player.particleColliders[i].cost){showDiv("particleCollider"+i);}}}
+function implodeReality(){if(player.particles>=player.implosion.cost){showDiv("implodedRealityButton");}if(player.implosion.timesImploded>=1){showDiv("implodedRealityButton");}}
+function revealImplodeRealityTab(){if(player.implosion.implodedParticles>=1){show("implodedRealityTab");}}
 function reveals(){
-  implodeReality();revealImplodeRealityTab();revealParticleColliders()
+  implodeReality();revealImplodeRealityTab();revealParticleColliders();canBuy();
 }setInterval(reveals, 50);
